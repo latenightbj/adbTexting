96415f650cc8b20eeac06c1183f8b40d65906095
BJ <bjgodinez+macair@gmail.com>
2021-08-16 09:02:35 -0500

    adb wireless (use the ip of the phone)

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import time
 
 # Default is "127.0.0.1" and 5037
 client = AdbClient(host="127.0.0.1", port=5037)
-device = client.device('DeviceName')
+#ip:port
+device = client.device('###ip:port###')
 
 
 def bomber(number):
     for i in range(number):
         device.shell(
-            f'am start -a android.intent.action.SENDTO -d sms:CCXXXXXXXXXX --es sms_body "{100 - i} bottles of '
+            f'am start -a android.intent.action.SENDTO -d sms:+CCXXXXXXXXXX --es sms_body "{100 - i} bottles of '
             f'beer on the wall {100 - i} bottles of beer... you take one down pass it around {100 - (i + 1)} bottles of beer on the wall"')
         time.sleep(.3)
         device.shell("input keyevent 22")
@@ -18,4 +19,4 @@ def bomber(number):
         time.sleep(5)
 
 
-bomber(100)
+bomber(2)
